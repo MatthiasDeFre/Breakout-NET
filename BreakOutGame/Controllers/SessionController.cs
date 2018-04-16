9eb325ada02722ed52585f6d611dae7c3c6d8796
JelleGeers <geers.jelle@gmail.com>
2018-04-16 18:03:12 +0200

    added Session Controller + view

---
+++ BreakOutGame/Controllers/SessionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BreakOutGame.Models.Domain.RepsitoryInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreakOutGame.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly IBoBSessionRepository _boBSessionRepository;
+        public IActionResult Index()
+        {
+            return View("SessionCodeScreen");
+        }
+
+        public SessionController(IBoBSessionRepository boBSessionRepository)
+        {
+            this._boBSessionRepository = boBSessionRepository;
+        }
+        
+    }
+}
